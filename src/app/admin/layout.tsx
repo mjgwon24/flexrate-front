45fe949c7dd2747783ff9380b33048c82c6739a5
mjgwon24 <alswlchlrh8@naver.com>
2025-05-11 22:01:42 +0900

    feat: AdminLayout - React Query Client Provider 추가

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,5 +1,15 @@
+'use client';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import React from 'react';
 import AdminLayout from '@/components/admin/AdminLayout/AdminLayout';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <AdminLayout>{children}</AdminLayout>;
+  const [queryClient] = React.useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AdminLayout>{children}</AdminLayout>
+    </QueryClientProvider>
+  );
 }
