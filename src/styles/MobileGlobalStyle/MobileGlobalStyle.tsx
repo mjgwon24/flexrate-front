dfd052b39095030be424079ece71ad1ca09b7ec3
seocylucky <seocylucky@github.com>
2025-05-12 17:43:18 +0900

    feat: mobile 뷰 GlobalStyle 작업 및 AppContainer 작업

---
+++ src/styles/MobileGlobalStyle/MobileGlobalStyle.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import { Global } from '@emotion/react';
+import { mobileGlobalStyleCss } from './MobileGlobalStyle.style';
+
+const MobileGlobalStyle = () => <Global styles={mobileGlobalStyleCss} />;
+
+export default MobileGlobalStyle;
