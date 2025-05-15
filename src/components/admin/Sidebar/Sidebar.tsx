b58752a5e4fb4bb730319100a583926aada63bae
mjgwon24 <alswlchlrh8@naver.com>
2025-05-15 14:19:07 +0900

    feat: 로그인한 유저 정보 반환 api 연결

--- src/components/admin/Sidebar/Sidebar.tsx
+++ src/components/admin/Sidebar/Sidebar.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 import { usePathname, useRouter } from 'next/navigation';
 
 import ProfileIcon from '@/assets/icons/profile_24.svg';
@@ -58,10 +60,53 @@ const menuSections: MenuSectionType[] = [
 ];
 
 const Sidebar = () => {
-  // 로그인한 사용자의 이름 반환 api
-  const userName = '숭멋사';
+  const [userName, setUserName] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
   const pathname = usePathname();
   const router = useRouter();
+
+  useEffect(() => {
+    const fetchUserName = async () => {
+      try {
+        setLoading(true);
+        setError(null);
+
+        const token = localStorage.getItem('accessToken');
+        if (!token) {
+          setError('인증 정보가 없습니다.');
+          setLoading(false);
+          return;
+        }
+
+        const response = await fetch('http://localhost:8080/api/members/mypage', {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json',
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error('사용자 정보를 불러오지 못했습니다.');
+        }
+
+        const data = await response.json();
+        setUserName(data.name);
+      } catch (err) {
+        if (err instanceof Error) {
+          setError(err.message || '알 수 없는 오류');
+        } else {
+          setError('알 수 없는 오류');
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUserName();
+  }, []);
 
   return (
     <SidebarWrapper>
@@ -69,7 +114,15 @@ const Sidebar = () => {
         <ProfileBox>
           <ProfileIcon width={28} height={28} />
           <ProfileGreeting>
-            <ProfileName>{userName}</ProfileName> 님, 반가워요!
+            {loading ? (
+              <p></p>
+            ) : error ? (
+              <p>error</p>
+            ) : (
+              <>
+                <ProfileName>{userName}</ProfileName> 님, 반가워요!
+              </>
+            )}
           </ProfileGreeting>
         </ProfileBox>
         <SidebarHr />
