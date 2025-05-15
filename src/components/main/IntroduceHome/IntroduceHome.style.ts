6e158952232091a1bf1f65253bfc402848500a9a
seocylucky <seocylucky@github.com>
2025-05-15 11:01:57 +0900

    feat: 메인/홈 화면(대출 안한 사람/비회원) UI 작업

---
+++ src/components/main/IntroduceHome/IntroduceHome.style.ts
@@ -0,0 +1,107 @@
+'use client';
+
+import { semanticColor } from '@/styles/colors';
+import { typoStyleMap } from '@/styles/typos';
+import styled from '@emotion/styled';
+
+import { motion } from 'framer-motion';
+
+export const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const IntroduceContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: fit-content;
+  padding: 0px 22px;
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient(180deg, #fff 0%, #d2ebff 100%);
+`;
+export const IntroduceTextContainer = styled.div`
+  display: flex;
+  margin: 20px 0px 5px 0px;
+  padding: 12px 0px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Title = styled.div`
+  ${typoStyleMap['title3']};
+  color: ${semanticColor.text.normal.primary};
+`;
+
+export const SubTitle = styled.div`
+  margin-top: 2px;
+  ${typoStyleMap['body2_m']}
+  color: ${semanticColor.text.normal.sub2};
+`;
+
+export const Icon3DContainer = styled.div`
+  width: 169px;
+  height: 169px;
+`;
+
+export const MainImage3D = styled(motion.div)`
+  position: absolute;
+`;
+
+export const ContentBox = styled(motion.div)`
+  display: flex;
+  width: 323px;
+  margin-top: 12px;
+  margin-bottom: 30px;
+  padding: 8px 10px;
+  flex-direction: column;
+  align-items: center;
+
+  border-radius: 12px;
+  background: ${semanticColor.bg.default};
+  box-shadow: 0px 4px 12px 4px rgba(0, 82, 147, 0.2);
+`;
+
+export const Content = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  padding: 12px 8px;
+  gap: 4px;
+  align-items: center;
+
+  border-bottom: 1px solid ${semanticColor.border.inactive.default};
+
+  &:last-child {
+    border-bottom: none;
+  }
+`;
+
+export const ContentTitle = styled.div`
+  ${typoStyleMap['caption1_m']};
+  color: ${semanticColor.text.normal.sub2};
+`;
+
+export const ContentSubTextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const ContentSubTextTitle = styled.div`
+  ${typoStyleMap['body2_sb']};
+  color: ${semanticColor.text.normal.sub1};
+`;
+
+export const ContentSubTextContent = styled.div`
+  ${typoStyleMap['caption2_r']};
+  color: ${semanticColor.text.normal.sub3};
+`;
+
+export const Spacing = styled.div`
+  height: 9px;
+`;
---
+++ src/components/main/IntroduceHome/IntroduceHome.tsx
@@ -0,0 +1,68 @@
+'use client';
+
+import Image from 'next/image';
+import {
+  Content,
+  ContentBox,
+  ContentSubTextContainer,
+  ContentSubTextContent,
+  ContentSubTextTitle,
+  ContentTitle,
+  Icon3DContainer,
+  IntroduceContentContainer,
+  IntroduceTextContainer,
+  MainImage3D,
+  Spacing,
+  SubTitle,
+  Title,
+  Wrapper,
+} from './IntroduceHome.style';
+import Button from '@/components/Button/Button';
+
+const IntroduceHome = ({ user }: { user: boolean }) => {
+  return (
+    <Wrapper>
+      <IntroduceContentContainer>
+        <IntroduceTextContainer>
+          <Title>FlexRate</Title>
+          <SubTitle>당신의 라이프스타일로 평가받는 신용대출</SubTitle>
+          <SubTitle>지금 FlexRate로 시작하세요!</SubTitle>
+        </IntroduceTextContainer>
+        <Icon3DContainer>
+          <MainImage3D
+            initial={{ opacity: 0, y: 30 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 0 }}
+          >
+            <Image src={'/imgs/mainImage.svg'} alt="대출 상품 이미지" width={169} height={169} />
+          </MainImage3D>
+        </Icon3DContainer>
+        <ContentBox
+          initial={{ opacity: 0, y: 30 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0 }}
+        >
+          <Content>
+            <ContentTitle>최고금리</ContentTitle>
+            <ContentSubTextContainer>
+              <ContentSubTextTitle>연 2.6%</ContentSubTextTitle>
+              <ContentSubTextContent>(개인, 세전, 금액 구간 별 최고 기준)</ContentSubTextContent>
+            </ContentSubTextContainer>
+          </Content>
+          <Content>
+            <ContentTitle>가입기간</ContentTitle>
+            <ContentSubTextTitle>1일 ~ 365일</ContentSubTextTitle>
+          </Content>
+          <Content>
+            <ContentTitle>가입금액</ContentTitle>
+            <ContentSubTextTitle>제한 없음</ContentSubTextTitle>
+          </Content>
+        </ContentBox>
+      </IntroduceContentContainer>
+      <Spacing />
+      <Button size="XL" text="대출 신청하기" varient="PRIMARY" />
+    </Wrapper>
+  );
+};
+
+export default IntroduceHome;
