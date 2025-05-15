9b1bc20d28d9f959ce78b83b147693ca33dc3445
mjgwon24 <alswlchlrh8@naver.com>
2025-05-15 15:52:03 +0900

    fix: fetch -> axios 함수 변경

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,17 +1,15 @@
+import axios from 'axios';
+
 /**
  * 인증 관련 API
  */
+const API_URL = process.env.API_URL || 'http://localhost:8080';
+
 // 로그인 유저 정보 반환
 export async function fetchMyPageUser(token: string) {
-  const res = await fetch('http://localhost:8080/api/members/mypage', {
-    method: 'GET',
-    headers: {
-      Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json',
-    },
+  const { data } = await axios.get(`${API_URL}/api/members/mypage`, {
+    headers: { Authorization: `Bearer ${token}` },
   });
 
-  if (!res.ok) throw new Error('사용자 정보를 불러오지 못했습니다.');
-
-  return res.json();
+  return data;
 }
