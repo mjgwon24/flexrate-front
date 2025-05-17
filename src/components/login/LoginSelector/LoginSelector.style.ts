d829e16c70d620d09757e30466e2871c1048f7ab
seocylucky <seocylucky@github.com>
2025-05-18 03:32:18 +0900

    feat: LoginSelector(로그인 수단 선택 화면) 컴포넌트 작업

---
+++ src/components/login/LoginSelector/LoginSelector.style.ts
@@ -0,0 +1,49 @@
+import { semanticColor } from '@/styles/colors';
+import { typoStyleMap } from '@/styles/typos';
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Title = styled.div`
+  padding: 12px 22px;
+  ${typoStyleMap['head1']};
+  color: ${semanticColor.text.normal.primary};
+`;
+
+export const Question = styled.div`
+  padding: 12px 0px 12px 0px;
+  ${typoStyleMap['title3']};
+  color: ${semanticColor.text.normal.primary};
+`;
+
+export const BtnWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  margin-top: 8px;
+`;
+
+export const SheetBtn = styled.button`
+  cursor: pointer;
+  width: 100%;
+  padding: 10px 0px;
+  ${typoStyleMap['body1_sb']};
+  border-radius: 12px;
+  border: 1px solid ${semanticColor.border.active.sub3};
+  background: ${semanticColor.bgBtn.active.wt};
+  color: ${semanticColor.text.normal.sub1};
+  display: flex;
+  justify-content: center;
+`;
+
+export const BtnContainer = styled.div`
+  width: 140px;
+  display: flex;
+  gap: 10px;
+  align-items: center;
+  justify-content: flex-start;
+`;
---
+++ src/components/login/LoginSelector/LoginSelector.tsx
@@ -0,0 +1,55 @@
+'use client';
+import React from 'react';
+import {
+  BtnContainer,
+  BtnWrapper,
+  Container,
+  Question,
+  SheetBtn,
+  Title,
+} from './LoginSelector.style';
+import BottomSheet from '@/components/BottomSheet/BottomSheet';
+import Image from 'next/image';
+
+export type LoginSelectorProps = {
+  onSelectPassword: () => void;
+  onSelectEmail: () => void;
+  onSelectFace: () => void;
+};
+
+const LoginSelector = ({ onSelectEmail, onSelectFace, onSelectPassword }: LoginSelectorProps) => {
+  return (
+    <Container>
+      <Title>
+        반가워요!
+        <br />
+        로그인을 진행할게요
+      </Title>
+      <BottomSheet isOpen={true}>
+        <Question>어떤 방법으로 로그인할까요?</Question>
+        <BtnWrapper>
+          <SheetBtn onClick={onSelectEmail}>
+            <BtnContainer>
+              <Image src={'/imgs/lock.svg'} width={36} height={36} alt="간편 비밀번호" />
+              간편 비밀번호
+            </BtnContainer>
+          </SheetBtn>
+          <SheetBtn onClick={onSelectFace}>
+            <BtnContainer>
+              <Image src={'/imgs/faceId.svg'} width={36} height={36} alt="FaceID" />
+              FACE ID
+            </BtnContainer>
+          </SheetBtn>
+          <SheetBtn onClick={onSelectPassword}>
+            <BtnContainer>
+              <Image src={'/imgs/pattern.svg'} width={36} height={36} alt="패턴" />
+              패턴
+            </BtnContainer>
+          </SheetBtn>
+        </BtnWrapper>
+      </BottomSheet>
+    </Container>
+  );
+};
+
+export default LoginSelector;
