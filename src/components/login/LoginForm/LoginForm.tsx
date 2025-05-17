5493130409bae2387fdface523b3f2ac156ee94b
seocylucky <seocylucky@github.com>
2025-05-18 03:32:31 +0900

    feat: LoginForm(로그인 화면) 컴포넌트 작업

---
+++ src/components/login/LoginForm/LoginForm.tsx
@@ -0,0 +1,122 @@
+'use client';
+import React, { useEffect, useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { authSchemas } from '@/schemas/auth.schema';
+import { Container, Title } from '../LoginSelector/LoginSelector.style';
+import TextField from '@/components/TextField/TextField';
+import Button from '@/components/Button/Button';
+import { BtnContainer, FormContainer } from './LoginForm.style';
+import { motion } from 'framer-motion';
+import { z } from 'zod';
+import { useRouter } from 'next/navigation';
+
+type LoginFormValues = z.infer<typeof authSchemas.login>;
+
+const LoginForm = () => {
+  const [emailEntered, setEmailEntered] = useState(false);
+  const router = useRouter();
+
+  const {
+    control,
+    handleSubmit,
+    trigger,
+    watch,
+    formState: { errors, isValid, dirtyFields },
+  } = useForm<LoginFormValues>({
+    resolver: zodResolver(authSchemas.login),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+    mode: 'onChange',
+  });
+
+  const email = watch('email');
+
+  useEffect(() => {
+    const checkEmailValid = async () => {
+      const valid = await trigger('email');
+      if (valid) {
+        setEmailEntered(true);
+      }
+    };
+
+    if (!emailEntered && email) {
+      checkEmailValid();
+    }
+  }, [email, emailEntered, trigger]);
+
+  const onSubmit = (data: LoginFormValues) => {
+    console.log('로그인 시도:', data);
+  };
+
+  return (
+    <Container>
+      <Title>
+        반가워요!
+        <br />
+        로그인을 진행할게요
+      </Title>
+
+      <FormContainer onSubmit={handleSubmit(onSubmit)}>
+        {emailEntered && (
+          <motion.div
+            initial={{ y: -40, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.4 }}
+          >
+            <Controller
+              name="password"
+              control={control}
+              render={({ field }) => (
+                <TextField
+                  value={field.value}
+                  onChange={field.onChange}
+                  isError={!!errors.password}
+                  rightContent={{
+                    type: 'DELETE',
+                    onClick: () => field.onChange(''),
+                  }}
+                >
+                  <TextField.TextFieldBox type="password" placeholder="비밀번호 입력" />
+                  <TextField.ErrorText message={errors.password?.message ?? ''} />
+                </TextField>
+              )}
+            />
+          </motion.div>
+        )}
+
+        <Controller
+          name="email"
+          control={control}
+          render={({ field }) => (
+            <TextField
+              value={field.value}
+              onChange={field.onChange}
+              isError={!!errors.email}
+              rightContent={{
+                type: 'DELETE',
+                onClick: () => field.onChange(''),
+              }}
+            >
+              <TextField.TextFieldBox type="email" placeholder="example@fisa.com" />
+              <TextField.ErrorText message={errors.email?.message ?? ''} />
+            </TextField>
+          )}
+        />
+
+        <BtnContainer>
+          <Button
+            type="submit"
+            text="로그인하기"
+            disabled={emailEntered ? !isValid : !dirtyFields.email || !!errors.email}
+            onClick={() => router.push('/')}
+          />
+        </BtnContainer>
+      </FormContainer>
+    </Container>
+  );
+};
+
+export default LoginForm;
