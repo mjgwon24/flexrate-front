4541a563c4d7e066e32b3a4cda5c6f8913ade2cf
HeoYeonGyu <hyg0107@gmail.com>
2025-05-18 21:59:12 +0900

    fix: ErrorBlock 컴포넌트로 분리

---
+++ src/components/admin/ErrorBlock/ErrorBlock.style.ts
@@ -0,0 +1,18 @@
+import styled from '@emotion/styled';
+
+export const ErrorWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 60vh;
+  width: 100%;
+  gap: 16px;
+  text-align: center;
+`;
+
+export const ErrorMessage = styled.div`
+  font-size: 16px;
+  color: #595959;
+  margin-bottom: 8px;
+`;
