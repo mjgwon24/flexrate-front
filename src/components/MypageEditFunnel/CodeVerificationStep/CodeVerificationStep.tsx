6e49d4e566ba8e4758dfc681c50a26158928b588
mjgwon24 <alswlchlrh8@naver.com>
2025-05-19 12:04:32 +0900

    feat: 내 정보 수정 code verification step UI 구현

--- src/components/MypageEditFunnel/CodeVerificationStep/CodeVerificationStep.tsx
+++ src/components/MypageEditFunnel/CodeVerificationStep/CodeVerificationStep.tsx
@@ -1,3 +1,14 @@
+import React from "react";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import Button from "@/components/Button/Button";
+import { BtnContainer, BtnWrapper } from "@/components/MypageEditFunnel/EmailStep/EmailStep.style";
+import { Title, MainContainer } from "@/components/MypageEditFunnel/MypageEditFunnel.style";
+import TextField from "@/components/TextField/TextField";
+import { SubContainer } from "@/app/mypage/page.style";
 
 interface Props {
   value: { email: string; code: string };
@@ -5,11 +16,86 @@ interface Props {
   onNext: () => void;
 }
 
+const codeSchema = z.object({
+  code: z
+    .string()
+    .nonempty("인증코드를 입력해주세요")
+});
+
+type CodeFormValues = z.infer<typeof codeSchema>;
+
 export const CodeVerificationStep = ({ value, onChange, onNext }: Props) => {
-  const handleChange = (v: string) => onChange({ ...value, code: v });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+    setValue,
+    watch,
+  } = useForm<CodeFormValues>({
+    resolver: zodResolver(codeSchema),
+    mode: "onChange",
+    defaultValues: {
+      code: value.code || "",
+    },
+  });
+
+  const watchedCode = watch("code");
+  React.useEffect(() => {
+    if (onChange) onChange({ email: value.email, code: watchedCode });
+  }, [watchedCode]);
+
+  const onSubmit = (data: CodeFormValues) => {
+    onNext();
+  };
 
   return (
     <>
+      <form onSubmit={handleSubmit(onSubmit)} style={{ width: "100%" }}>
+        <MainContainer>
+          <Title>이메일 인증을 시작할게요</Title>
+
+          <SubContainer>
+            <TextField
+              value={watchedCode}
+              onChange={(v: string) => setValue("code", v, { shouldValidate: true })}
+              isError={!!errors.code}
+              rightContent={{
+                type: "DELETE",
+                onClick: () => setValue("code", "", { shouldValidate: true }),
+              }}
+            >
+              <TextField.TextFieldBox
+                type="text"
+                placeholder="인증번호 입력"
+                {...register("code")}
+              />
+              {errors.code && (
+                <TextField.ErrorText message={errors.code.message || ""} />
+              )}
+            </TextField>
+          </SubContainer>
+
+          <SubContainer>
+            <TextField
+              value={value?.email}
+              onChange={() => {}}
+              isDisabled={true}
+            >
+              <TextField.TextFieldBox placeholder={value?.email} />
+            </TextField>
+          </SubContainer>
+        </MainContainer>
+
+        <BtnWrapper>
+          <BtnContainer>
+            <Button
+              text="인증하기"
+              type="submit"
+              disabled={!isValid}
+            />
+          </BtnContainer>
+        </BtnWrapper>
+      </form>
     </>
   );
 };
