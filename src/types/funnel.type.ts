7a4a0f6b00fda208f3bc7baa922ba2f1788768f4
seocylucky <seocylucky@github.com>
2025-05-19 17:38:55 +0900

    feat: Signup funnel 타입 정의

---
+++ src/types/funnel.type.ts
@@ -0,0 +1,48 @@
+export type SignupSteps = {
+  이메일인증: { email?: string; verifyPassword?: number };
+  비밀번호설정: { email: string; password?: string };
+  로그인수단설정: { email: string; password: string; method?: string };
+  간편비밀번호설정: {
+    email: string;
+    password: string;
+    method: '간편비밀번호';
+  };
+  내정보입력: {
+    email: string;
+    password: string;
+    method: string;
+    gender?: string;
+    birthDate?: number;
+    name?: string;
+  };
+
+  소비성향체크: {
+    email: string;
+    password: string;
+    method: string;
+    gender: string;
+    birthDate: number;
+    name: string;
+    agreement?: boolean;
+  };
+  소비성향결과: {
+    email: string;
+    password: string;
+    method: string;
+    gender: string;
+    birthDate: number;
+    name: string;
+    agreement: boolean;
+  };
+  소비목적결과: {
+    email: string;
+    password: string;
+    method: string;
+    gender: string;
+    birthDate: number;
+    name: string;
+    agreement: boolean;
+    cosumptionType?: string;
+    consumptionGoal?: string;
+  };
+};
