fa138be0ab392075e3b7da6880ed59110981625e
seocylucky <seocylucky@github.com>
2025-05-20 10:49:59 +0900

    feat: 신용 정보 평가 정보 제공 동의 UI 작업

---
+++ src/components/creditEvaluationStep/AgreementCredit.style.ts
@@ -0,0 +1,27 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  padding: 0px 22px;
+  align-items: flex-start;
+`;
+export const TitleContainer = styled.div`
+  width: 100%;
+  margin-top: 67px 0px 26px 0px;
+`;
+
+export const CheckBoxContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const SmallCheckBoxes = styled.div`
+  display: flex;
+  flex-direction: column;
+  border-top: 1px solid #ddd;
+  padding-top: 16px;
+  gap: 22px;
+`;
