1a9a592e58f3424bd406e814fd097990c5231363
seocylucky <seocylucky@github.com>
2025-05-21 23:04:23 +0900

    feat: Banner borderNone 타입 추가

--- src/components/Banner/Banner.style.ts
+++ src/components/Banner/Banner.style.ts
@@ -2,24 +2,25 @@ import styled from '@emotion/styled';
 
 import { semanticColor } from '@/styles/colors';
 
-export const BannerContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-    border: 1px solid ${semanticColor.border.inactive.default};
-    padding: 9px 21px;
-    border-radius: 15px;
-    min-width: fit-content;
-    background: ${semanticColor.bg.default};
+export const BannerContainer = styled.div<{ $borderNone?: boolean }>`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  border: ${({ $borderNone }) =>
+    $borderNone ? 'none' : `1px solid ${semanticColor.border.inactive.default}`};
+  padding: 9px 21px;
+  border-radius: 15px;
+  min-width: fit-content;
+  background: ${semanticColor.bg.default};
 `;
 
 export const BannerContent = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    width: 255px;
-    min-width: fit-content;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  width: 255px;
+  min-width: fit-content;
 `;
 
 interface BannerLabelProps {
@@ -42,9 +43,9 @@ export const BannerLabel = styled.div<BannerLabelProps>`
 `;
 
 export const BannerDesc = styled.p`
-    font-size: 14px;
-    font-weight: 600;
-    margin: 0;
-    color: ${semanticColor.text.normal.primary};
-    min-width: fit-content;
+  font-size: 14px;
+  font-weight: 600;
+  margin: 0;
+  color: ${semanticColor.text.normal.primary};
+  min-width: fit-content;
 `;
