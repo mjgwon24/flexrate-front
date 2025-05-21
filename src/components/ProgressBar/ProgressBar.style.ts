ece7b18192031c156aa11356544d454086e0bf92
seocylucky <seocylucky@github.com>
2025-05-21 23:03:22 +0900

    feat: ProgressBar type 속성 추가

--- src/components/ProgressBar/ProgressBar.style.ts
+++ src/components/ProgressBar/ProgressBar.style.ts
@@ -1,21 +1,24 @@
-import { semanticColor } from '@/styles/colors';
+import { primitiveColor, semanticColor } from '@/styles/colors';
+import { typoStyleMap } from '@/styles/typos';
 import styled from '@emotion/styled';
 import { motion } from 'framer-motion';
 
-export const ProgressBarContainer = styled.div`
-  padding: 8px 24px;
+export const ProgressBarContainer = styled.div<{ type?: string }>`
+  padding: ${({ type }) => (type === 'MAIN' ? '' : '8px 24px')};
 `;
 
-export const ProgressBarBackground = styled.div`
+export const ProgressBarBackground = styled.div<{ type?: string }>`
   width: 100%;
-  height: 3px;
-  background-color: ${semanticColor.bgBtn.inactive.default};
+  height: ${({ type }) => (type === 'MAIN' ? 10 : 3)}px;
+  background-color: ${({ type }) =>
+    type === 'MAIN' ? primitiveColor.blue[50] : semanticColor.bgBtn.inactive.default};
   border-radius: 100px;
 `;
 
-export const ProgressBarFill = styled(motion.div)`
+export const ProgressBarFill = styled(motion.div)<{ type?: string }>`
   height: 100%;
-  background-color: ${semanticColor.bgBtn.active.primary};
+  background-color: ${({ type }) =>
+    type === 'MAIN' ? primitiveColor.blue[700] : semanticColor.bgBtn.active.primary};
   border-radius: 100px;
   width: 0%;
 `;
--- src/components/ProgressBar/ProgressBar.tsx
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -2,17 +2,19 @@ import { motion } from 'framer-motion';
 import { ProgressBarBackground, ProgressBarContainer, ProgressBarFill } from './ProgressBar.style';
 
 interface ProgressBarProps {
+  type?: 'MAIN' | 'DEFAULT';
   totalSteps: number;
   currentStep: number;
 }
 
-const ProgressBar = ({ totalSteps, currentStep }: ProgressBarProps) => {
+const ProgressBar = ({ type, totalSteps, currentStep }: ProgressBarProps) => {
   const progress = totalSteps === 0 ? 0 : (currentStep / totalSteps) * 100;
 
   return (
-    <ProgressBarContainer>
-      <ProgressBarBackground>
+    <ProgressBarContainer type={type}>
+      <ProgressBarBackground type={type}>
         <ProgressBarFill
+          type={type}
           animate={{ width: `${progress}%` }}
           transition={{ duration: 0.3, ease: 'easeOut' }}
         />
