270c9f0c6c537c43f660e3b1a1283928c83c53ec
seocylucky <seocylucky@github.com>
2025-05-22 17:54:12 +0900

    feat: queryClient 설정 및 layout에 providers 설정

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -2,18 +2,10 @@
 
 import React from 'react';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
 import AdminLayout from '@/components/admin/AdminLayout/AdminLayout';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  const [queryClient] = React.useState(() => new QueryClient());
-
-  return (
-    <QueryClientProvider client={queryClient}>
-      <AdminLayout>{children}</AdminLayout>
-    </QueryClientProvider>
-  );
+  return <AdminLayout>{children}</AdminLayout>;
 };
 
 export default Layout;
