7b7ca7b13e379b7e4a599687a141ef0164a9ab13
seocylucky <seocylucky@github.com>
2025-05-22 17:47:50 +0900

    remove: 필요없는 파일 삭제

--- src/utils/reverseMap.ts
+++
@@ -1,6 +0,0 @@
-function reverseMap<T extends Record<string, string>>(map: T, value: string): keyof T | undefined {
-  // 소비목적 객체의 [key, value] 쌍 중 value가 주어진 값과 일치하는 항목을 찾고, 해당 key를 반환
-  return (Object.entries(map).find(([, v]) => v === value)?.[0]) as keyof T | undefined;
-}
-
-export default reverseMap;
