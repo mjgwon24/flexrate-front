1e7f17f9163733a1ee04151f3d1f3f7a3fefb129
seocylucky <seocylucky@github.com>
2025-05-28 10:47:48 +0900

    feat: 대출 신청 관련 타입 설정 및 API 호출 함수 작성, 대출 심사 신청 API 연동

--- src/components/loanApplicationFunnel/ReviewResultAndLoanApplication/ReviewResultAndLoanApplication.tsx
+++ src/components/loanApplicationFunnel/ReviewResultAndLoanApplication/ReviewResultAndLoanApplication.tsx
@@ -1,5 +1,12 @@
+'use client';
+
+import Button from '@/components/Button/Button';
 import TextField from '@/components/TextField/TextField';
+import { useGetLoanReivewApplication } from '@/hooks/useLoanApplication';
+import { formatNumberComma } from '@/utils/formatNumberComma';
+
 import { FunnelContextMap } from '../LoanApplicationFunnel';
+
 import {
   BtnContainer,
   Caption,
@@ -20,7 +27,6 @@ import {
   TableItemKey,
   TableItemValue,
 } from './ReviewResultAndLoanApplication.style';
-import Button from '@/components/Button/Button';
 
 interface ReviewResultProps {
   value: FunnelContextMap['대출신청접수'];
@@ -29,85 +35,99 @@ interface ReviewResultProps {
 }
 
 const ReviewResultAndLoanApplication = ({ value, onChange, onSubmit }: ReviewResultProps) => {
+  const token = typeof window !== undefined ? localStorage.getItem('accessToken') ?? '' : '';
+  const { data: result } = useGetLoanReivewApplication(token);
+
+  const loanLimit = result ? formatNumberComma(result?.loanLimit) : '';
+
   return (
-    <Container>
-      <MainContainer>
-        <SubTitle>상세 내용</SubTitle>
-        <TableContainer>
-          <TableItem>
-            <TableItemKey>성명</TableItemKey>
-            <TableItemValue>Fisa</TableItemValue>
-          </TableItem>
-          <TableItem>
-            <TableItemKey>대출 심사 일자</TableItemKey>
-            <TableItemValue>2023.10.03</TableItemValue>
-          </TableItem>
-          <TableItem>
-            <TableItemKey>대출 가능 한도</TableItemKey>
-            <TableItemValue>
-              <TableItemValue $isStrong={true}>3,000,000,000</TableItemValue>원
-            </TableItemValue>
-          </TableItem>
-          <TableItem>
-            <TableItemKey>초기 대출 금리</TableItemKey>
-            <TableItemValue>
-              연 <TableItemValue $isStrong={true}>14.5%</TableItemValue>
-            </TableItemValue>
-          </TableItem>
-          <TableItem>
-            <TableItemKey>금리 범위</TableItemKey>
-            <TableItemValue>
-              연 <TableItemValue $isStrong={true}>12.5% ~ 15.1%</TableItemValue>
-            </TableItemValue>
-          </TableItem>
-          <CaptionContainer>
-            <Caption>· 기준금리 4.25% + 가산금리 10.35% - 우대금리 0.1%</Caption>
-            <br />
-            <Caption>
-              · 위 대출금리는 현재 약정 시 조건이며, 대출 실행일의 기준금리에 따라 변동될 수
-              있습니다.
-            </Caption>
-          </CaptionContainer>
-        </TableContainer>
-        <LoanApplicationContainer>
-          <SubTitle>대출 신청 접수</SubTitle>
-          <Description>
-            대출 가능 한도 내에서
-            <br />
-            대출 신청 금액을 조정하 기입해주세요
-          </Description>
-          <TextField
-            value={value.loanAmount.toString()}
-            onChange={(updatedValue) =>
-              onChange({ ...value, loanAmount: Number(updatedValue) || 0 })
-            }
-          >
-            <TextField.Label helperText="단위: 원">대출 금액</TextField.Label>
-            <TextField.TextFieldBox placeholder="숫자만 입력하세요" />
-          </TextField>
-          <SubDescriptionContainer>
-            <SubDescription>
-              대출 금액은 <SubDescription $isStrong={true}>최대 한도 금액</SubDescription>까지
-              가능합니다
-            </SubDescription>
-          </SubDescriptionContainer>
-          <MiniTitle>대출 상환 기간</MiniTitle>
-          <SliderContainer>
-            <SliderTitle>{value.repaymentMonth}개월</SliderTitle>
-            <Slider
-              type="range"
-              min={1}
-              max={12}
-              value={value.repaymentMonth}
-              onChange={(e) => onChange({ ...value, repaymentMonth: Number(e.target.value) || 0 })}
-            ></Slider>
-          </SliderContainer>
-        </LoanApplicationContainer>
-      </MainContainer>
-      <BtnContainer>
-        <Button text="대출 신청하기" onClick={onSubmit} />
-      </BtnContainer>
-    </Container>
+    result && (
+      <Container>
+        <MainContainer>
+          <SubTitle>상세 내용</SubTitle>
+          <TableContainer>
+            <TableItem>
+              <TableItemKey>성명</TableItemKey>
+              <TableItemValue>{result.name}</TableItemValue>
+            </TableItem>
+            <TableItem>
+              <TableItemKey>대출 심사 일자</TableItemKey>
+              <TableItemValue>{result.screeningDate}</TableItemValue>
+            </TableItem>
+            <TableItem>
+              <TableItemKey>대출 가능 한도</TableItemKey>
+              <TableItemValue>
+                <TableItemValue $isStrong={true}>{loanLimit}</TableItemValue>원
+              </TableItemValue>
+            </TableItem>
+            <TableItem>
+              <TableItemKey>초기 대출 금리</TableItemKey>
+              <TableItemValue>
+                연 <TableItemValue $isStrong={true}>{result.initialRate}%</TableItemValue>
+              </TableItemValue>
+            </TableItem>
+            <TableItem>
+              <TableItemKey>금리 범위</TableItemKey>
+              <TableItemValue>
+                연{' '}
+                <TableItemValue $isStrong={true}>
+                  {result?.rateRangeFrom}% ~ {result.rateRangeTo}%
+                </TableItemValue>
+              </TableItemValue>
+            </TableItem>
+            <CaptionContainer>
+              <Caption>· 기준금리 4.25% + 가산금리 10.35% - 우대금리 0.1%</Caption>
+              <br />
+              <Caption>
+                · 위 대출금리는 현재 약정 시 조건이며, 대출 실행일의 기준금리에 따라 변동될 수
+                있습니다.
+              </Caption>
+            </CaptionContainer>
+          </TableContainer>
+          <LoanApplicationContainer>
+            <SubTitle>대출 신청 접수</SubTitle>
+            <Description>
+              대출 가능 한도 내에서
+              <br />
+              대출 신청 금액을 조정하 기입해주세요
+            </Description>
+            <TextField
+              value={value.loanAmount.toString()}
+              onChange={(updatedValue) => {
+                if (/^\d*$/.test(updatedValue)) {
+                  onChange({ ...value, loanAmount: Number(updatedValue) || 0 });
+                }
+              }}
+            >
+              <TextField.Label helperText="단위: 원">대출 금액</TextField.Label>
+              <TextField.TextFieldBox placeholder="숫자만 입력하세요" />
+            </TextField>
+            <SubDescriptionContainer>
+              <SubDescription>
+                대출 금액은 <SubDescription $isStrong={true}>최대 한도 금액</SubDescription>까지
+                가능합니다
+              </SubDescription>
+            </SubDescriptionContainer>
+            <MiniTitle>대출 상환 기간</MiniTitle>
+            <SliderContainer>
+              <SliderTitle>{value.repaymentMonth}개월</SliderTitle>
+              <Slider
+                type="range"
+                min={1}
+                max={12}
+                value={value.repaymentMonth}
+                onChange={(e) =>
+                  onChange({ ...value, repaymentMonth: Number(e.target.value) || 0 })
+                }
+              ></Slider>
+            </SliderContainer>
+          </LoanApplicationContainer>
+        </MainContainer>
+        <BtnContainer>
+          <Button text="대출 신청하기" onClick={onSubmit} />
+        </BtnContainer>
+      </Container>
+    )
   );
 };
 
