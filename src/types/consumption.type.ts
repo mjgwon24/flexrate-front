c30be2274a25b5c8645c790ca51d9e839abf2723
seocylucky <seocylucky@github.com>
2025-06-01 15:31:10 +0900

    feat: consumption report 관련 타입 설정

---
+++ src/types/consumption.type.ts
@@ -0,0 +1,39 @@
+import { LargeNumberLike } from 'crypto';
+
+export const categoryMap: Record<string, string> = {
+  FOOD: '식비',
+  LIVING: '주거/생활',
+  LEISURE: '여가/취미',
+  TRANSPORT: '교통',
+  COMMUNICATION: '통신',
+  EDUCATION: '교육',
+  HEALTH: '의료/건강',
+  ETC: '기타',
+};
+
+export interface ReportMonth {
+  year: number;
+  month: string;
+  monthValue: number;
+  leapYear: boolean;
+}
+
+export interface GetConsumptionReportResponse {
+  reportId: number;
+  memberId: number;
+  reportMonth: ReportMonth;
+  summary: string;
+  createdAt: string;
+}
+
+export interface stats {
+  category: string;
+  amount: number;
+  percentage: number;
+}
+
+export interface GetConsumptionStaticsticResponse {
+  memberId: number;
+  month: ReportMonth;
+  stats: stats[];
+}
