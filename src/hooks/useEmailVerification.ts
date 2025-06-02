03a1838361af9c5e552145b77259eb58948ecc97
yyc0026 <youngchan206@gmail.com>
2025-06-02 10:32:07 +0900

    fix: 회원가입 플로우

--- src/hooks/useEmailVerification.ts
+++ src/hooks/useEmailVerification.ts
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 
 import { useMutation } from '@tanstack/react-query';
-import axios, { AxiosError, isAxiosError } from 'axios';
 
 import { postSendEmailVerificationCode } from '@/apis/auth';
 
@@ -10,14 +9,9 @@ export const useEmailVerification = () => {
 
   const { mutate, isPending } = useMutation({
     mutationFn: postSendEmailVerificationCode,
-    onError: (error: unknown) => {
-      if (isAxiosError(error) && error.response?.status === 409) {
-        alert('이미 가입된 이메일입니다.');
-        setCodeSent(false);
-      } else {
-        alert('인증메일 발송에 실패했습니다.');
-      }
-    }
+    onError: () => {
+      alert('인증메일 발송에 실패했습니다.');
+    },
   });
 
   const requestCode = (email: string) => {
