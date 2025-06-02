1c5d7e915791aa2da7266b8b7ef0187d4f0d41c7
mjgwon24 <alswlchlrh8@naver.com>
2025-06-02 11:34:37 +0900

    feat: 체크박스 컴포넌트에 추가 동의 옵션 및 링크 추가

--- src/components/creditEvaluationStep/AgreementCredit/AgreementCredit.tsx
+++ src/components/creditEvaluationStep/AgreementCredit/AgreementCredit.tsx
@@ -31,17 +31,23 @@ const AgreementCredit = ({ onNext }: AgreementCreditProps) => {
     mode: 'onChange',
     defaultValues: {
       agreePrivacy: false,
-      agreeService: false,
+      agreeMyDataTerms: false,
+      agreeThird: false,
+      agreeCreditScoreGuide: false,
     },
   });
 
   const agreePrivacy = watch('agreePrivacy');
-  const agreeService = watch('agreeService');
-  const allChecked = agreePrivacy && agreeService;
+  const agreeThird = watch('agreeThird');
+  const agreeMyDataTerms = watch('agreeMyDataTerms');
+  const agreeCreditScoreGuide = watch('agreeCreditScoreGuide');
+  const allChecked = agreePrivacy;
 
   const handleAllChange = (nextChecked: boolean) => {
     setValue('agreePrivacy', nextChecked, { shouldValidate: true });
-    setValue('agreeService', nextChecked, { shouldValidate: true });
+    setValue('agreeThird', nextChecked, { shouldValidate: true });
+    setValue('agreeMyDataTerms', nextChecked, { shouldValidate: true });
+    setValue('agreeCreditScoreGuide', nextChecked, { shouldValidate: true });
   };
   return (
     <Container>
@@ -63,18 +69,38 @@ const AgreementCredit = ({ onNext }: AgreementCreditProps) => {
         <SmallCheckBoxes>
           <CheckBox
             size="small"
-            label="개인 정보 제공 동의"
+            label="[필수] 개인(신용)정보 수집·이용·제공 동의서"
             showMoreText="더보기"
+            showMoreUrl="https://ryuseunghan.notion.site/202c3966a1438047bbf4eedd02078ea1?source=copy_link"
             checked={agreePrivacy}
             onChange={(val) => setValue('agreePrivacy', val, { shouldValidate: true })}
           />
 
           <CheckBox
             size="small"
-            label="서비스 이용 약관 동의"
+            label="[필수] 우리 마이데이터 서비스 이용약관"
             showMoreText="더보기"
-            checked={agreeService}
-            onChange={(val) => setValue('agreeService', val, { shouldValidate: true })}
+            showMoreUrl="https://ryuseunghan.notion.site/204c3966a143806593d8cd4b482d0078?source=copy_link"
+            checked={agreeMyDataTerms}
+            onChange={(val) => setValue('agreeMyDataTerms', val, { shouldValidate: true })}
+          />
+
+          <CheckBox
+            size="small"
+            label="개인(신용) 정보 제3자 제공 동의서"
+            showMoreText="더보기"
+            showMoreUrl="https://ryuseunghan.notion.site/3-204c3966a143804ebc07f14d76aa1d4c?source=copy_link"
+            checked={agreeThird}
+            onChange={(val) => setValue('agreeThird', val, { shouldValidate: true })}
+          />
+
+          <CheckBox
+            size="small"
+            label="신용 평가 지표 설명서"
+            showMoreText="더보기"
+            showMoreUrl="https://ryuseunghan.notion.site/204c3966a14380a4bc5efa062d0150b6?source=copy_link"
+            checked={agreeCreditScoreGuide}
+            onChange={(val) => setValue('agreeCreditScoreGuide', val, { shouldValidate: true })}
           />
         </SmallCheckBoxes>
 
